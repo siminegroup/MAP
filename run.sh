2e5948c21c9d65e40f24faf3566500ad0d63d7c8
ata-madanchi <54647628+ata-madanchi@users.noreply.github.com>
2025-10-15 16:37:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=0-120:00
 #SBATCH --array=0
-source /home/ataminer/envMAP/bin/activate
+source /envMAP/bin/activate
 
 # WORLD_SIZE as gpus/node * num_nodes
 export WORLD_SIZE=1
@@ -14,5 +14,6 @@ echo "NODELIST="${SLURM_NODELIST}
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
+
 
 python ./main.py --run_num=$SLURM_ARRAY_TASK_ID
